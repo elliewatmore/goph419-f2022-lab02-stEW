6183968579042869b4828e19abd5efde89071e53
elliewatmore <eleanor.watmore@ucalgary.ca>
2022-12-07 12:45:56 -0700

    fitted spline functions for density vs temperature data

--- source/driver.py
+++ source/driver.py
@@ -76,7 +76,7 @@ ax[1][2].plot(x2,y2, 'g^')
 ax[1][2].set_xlabel('Temperature (°C)')
 ax[1][2].set_ylabel('Air density (kg/m^3)')
 ax[1][2].set_title('Air density vs temp (cubic spline function)')
-plt.show()
+
 plt.savefig("Air/water density vs temp")
 
 
